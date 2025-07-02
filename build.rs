8a2101661753faa4e7f2931a879914229b5f4e86
echo000 <08finncam@gmail.com>
2025-07-02 01:39:09 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,17 +1,19 @@
-use embed_manifest::{
-    embed_manifest,
-    manifest::{HeapType, Setting},
-    new_manifest,
-};
 #[cfg(target_os = "windows")]
 fn main() {
-        embed_manifest(
-            new_manifest("echo000.Cast")
-                .heap_type(HeapType::SegmentHeap)
-                .long_path_aware(Setting::Enabled),
-        )
-        .expect("unable to embed manifest file");
+    use embed_manifest::{
+        embed_manifest,
+        manifest::{HeapType, Setting},
+        new_manifest,
+    };
+    embed_manifest(
+        new_manifest("echo000.Cast")
+            .heap_type(HeapType::SegmentHeap)
+            .long_path_aware(Setting::Enabled),
+    )
+    .expect("unable to embed manifest file");
     println!("cargo:rerun-if-changed=build.rs");
 }
+
 #[cfg(not(target_os = "windows"))]
 fn main() {}
+
